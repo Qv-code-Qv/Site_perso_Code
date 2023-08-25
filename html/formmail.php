f4ac0a6e38a2ec8d1069f9f37d8d9645c25a76a7
Quentin Vilcoque <q.vilcoque@gmail.com>
2023-08-25 17:44:15 +0200

    Update formmail.php

--- html/formmail.php
+++ html/formmail.php
@@ -1,11 +1,29 @@
 <?php
 
-header('refresh:3; ../index.html');
+//header('refresh:3; ../index.html');
 
-if (isset($_POST['message'])) {
-	$retour = mail('q.vilcoque@gmail.com', 'Envoi depuis la page Contact', $_POST['message'], '' . "\r\n" . 'Reply-to: ' . $_POST['email']);
-	if($retour)
-		echo '<p>Votre message a bien été envoyé.</p>';
+$message = ''; // Variable pour stocker les messages de succès ou d'erreur
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+	$email = $_POST['email'];
+	$subject = $_POST['objet'];
+	$messageBody = $_POST['message'];
+
+	// Validation rapide (vous pouvez ajouter plus de vérifications)
+	if (empty($email) || empty($objet) || empty($messageBody)) {
+		$message = "Veuillez remplir tous les champs.";
+	} else {
+		// Adresse email où vous souhaitez recevoir les messages
+		$to = "q.vilcoque@gmail.com";
+		$headers = "From: $email";
+
+		if (mail($to, $objet, $messageBody, $headers)) {
+			$message = "Message envoyé avec succès !";
+		} else {
+			$message = "Erreur lors de l'envoi du message.";
+		}
+	}
 }
 
 ?>
