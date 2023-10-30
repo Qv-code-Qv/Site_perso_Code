b7631ccbdd40da8d9d9a782b1fa8ce1ea9ebae5f
Quentin Vilcoque <q.vilcoque@gmail.com>
2023-10-30 15:35:00 +0100

    update

--- html/formmail.php
+++ html/formmail.php
@@ -1,7 +1,7 @@
 <?php
 
 //si le bouton envoyer a été cliqué
-if (isset($_POST["submit"])) {
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
     //on recupère l'adresse email
     $email = $_POST["email"];
     //on recupère l'objet du message
@@ -9,16 +9,34 @@ if (isset($_POST["submit"])) {
     //on recupère le message
     $message = $_POST["message"];
 
-    $to = "q.vilcoque@gmail.com";
-    $headers = "From:" . $email;
-    //on envoie le message avec la fonction mail
-    if (mail($to, $objet, $message, $headers))
-    //si le message a été envoyé, on le confirme
-    {
-        header('refresh:1; ../index.html');
+    if (empty($email) || empty($objet) || empty($message)) {
+        $message = "Veuillez remplir tous les champs.";
+    }else{
+
+        $to = "q.vilcoque@gmail.com";
+        $headers = "From:" . $email;
+        //on envoie le message avec la fonction mail
+        if (mail($to, $objet, $message, $headers))
+        //si le message a été envoyé, on le confirme
+        {
+            header('refresh:1; ../index.html');
+        }
+        //sinon on n'affiche un message d'erreur
+        else {
+            echo "Une erreur s'est produite";
+        }
+
     }
-    //sinon on n'affiche un message d'erreur
-    else {
-        echo "Une erreur s'est produite";
-    }
+
+   
 }
+
+
+
+?>
+
+
+
+
+
+
